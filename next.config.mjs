48f529550257a3ab50c19a268a6cf92d28a854b1
Shahzaman3 <faisalshahzaman8@gmail.com>
2026-05-18 15:23:35 +0530

    image bug fixed

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,14 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: 'images.unsplash.com' },
+      { protocol: 'https', hostname: 'upload.wikimedia.org' },
+      { protocol: 'https', hostname: 'lh3.googleusercontent.com' },
+      { protocol: 'https', hostname: 'commons.wikimedia.org' },
+    ],
+  },
 };
 
 export default nextConfig;
